88165d5673cde422dae32b35123bc4d344427abf
Tiago Carvalho <warkdem@gmail.com>
2024-12-28 11:37:18 -0300

    op

--- plugnplay/8_side_walk.gd
+++ plugnplay/8_side_walk.gd
@@ -1,7 +1,7 @@
 extends Node
 class_name side_scroller_walk
-## TODO: Implementar tweens
 
+@export_category("Geral")
 @export var speed := 200
 @export var host : CharacterBody2D
 
@@ -9,30 +9,48 @@ var can_walk := true
 var active := false
 var input := Vector2.ZERO
 var velocity := Vector2.ZERO
-var final_velocity := Vector2.ZERO
 var previous_input := Vector2.ZERO
 signal started
 signal ended
 var tween = create_tween()
 
+@export_category("Ramp Up")
+
+@export_enum("TRANS_LINEAR", "TRANS_SINE", "TRANS_QUINT", 
+	"TRANS_QUART", "TRANS_QUAD", "TRANS_EXPO", "TRANS_ELASTIC", 
+	"TRANS_CUBIC", "TRANS_CIRC", "TRANS_BOUNCE", "TRANS_BACK", 
+	"TRANS_SPRING") var rTRANS: int
+
+@export_enum("EASE_IN", "EASE_OUT", 
+	"EASE_IN_OUT", "EASE_OUT_IN" ) var rEase: int
+
+@export_category("Fall Off")
+
+@export_enum("TRANS_LINEAR", "TRANS_SINE", "TRANS_QUINT", 
+	"TRANS_QUART", "TRANS_QUAD", "TRANS_EXPO", "TRANS_ELASTIC", 
+	"TRANS_CUBIC", "TRANS_CIRC", "TRANS_BOUNCE", "TRANS_BACK", 
+	"TRANS_SPRING") var fTRANS: int
+
+@export_enum("EASE_IN", "EASE_OUT", 
+	"EASE_IN_OUT", "EASE_OUT_IN" ) var fEase: int
+
 func ramp_up():
 	tween.kill()
 	tween = create_tween()
 	tween.tween_property(
-		self, "velocity", input*speed, 0.01).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
+		self, "velocity", input*speed, 0.01).set_trans(rTRANS).set_ease(rEase)
 	tween.play()
 
 func fall_off():
 	tween.kill()
 	tween = create_tween()
 	tween.tween_property(
-		self, "velocity", Vector2.ZERO,0.4).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
+		self, "velocity", Vector2.ZERO,0.4).set_trans(fTRANS).set_ease(fEase)
 	tween.play()
 
 func _ready():
 	host = $"..".host
-	#started.connect(ramp_up)
-	#ended.connect(fall_off)
+
 func _physics_process(_delta: float) -> void:
 	input = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	# Verifica se o input mudou
--- plugnplay/movment.gd
+++ plugnplay/movment.gd
@@ -2,20 +2,16 @@ extends Node
 class_name movment
 
 @export var host := CharacterBody2D
-@export var varray := []
+var varray := []
 var vsum := Vector2.ZERO
 
 func sum():
 	vsum = Vector2.ZERO
 	for x in varray.size():
 		vsum += varray[x].velocity
-
 	return vsum
 
 func _ready() -> void:
 	for x in self.get_children().size():
 		self.get_child(x).started.connect(func(node) : varray.append(node))
 		self.get_child(x).ended.connect(func(node) : varray.erase(node))
-
-func _physics_process(_delta: float) -> void:
-	pass
